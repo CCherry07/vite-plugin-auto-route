6470331fc3a10e7a9a9cd2bf9699b9353dbbadbc
CCherry07 <2405693142@qq.com>
2023-01-30 23:40:47 +0800

    chore:  update TODO

--- plugin/index.ts
+++ plugin/index.ts
@@ -61,25 +61,7 @@ const VitePluginAutoRoute = (options?: Options): Plugin => {
 
     configureServer(server) {
       server.middlewares.use((req, res, next) => {
-        // handle router
-        // @ts-ignore
-        // if (req.url.includes('router') && !req.url.includes('node_modules')) {
-        //   const routerPath = path.resolve(process.cwd(), `.${req.url!}`)
-        //   console.log(routerPath)
-        //   const context = fs.readFileSync(routerPath, 'utf-8')
-        //   const reg = /(?<=routes: \[)([\s\S]*?)(?=\])/g
-        //   // 匹配 import 语句
-        //   const importReg = /(?<=import\()(.*?)(?=\))/g
-        //   const importList = context.match(importReg)?.map(item => item.slice(1, -1))
-        //   // const routeList = context.match(reg)?.[0].split(',').map(item => item.trim())
-        //   // 根据文件名 创建文件
-        //   importList?.forEach(item => {
-        //     const filePath = path.resolve(process.cwd(), `./src/${item}`)
-        //     if (!fs.existsSync(filePath)) {
-        //       writeFileRecursive(`./src/${item}`, template.replace("name", item))
-        //     }
-        //   })
-        // }
+        // TODO: 根据请求的data，生成对应的路由，及其文件
         next()
       })
     }
